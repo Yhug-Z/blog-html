33d0703d43eec3c67893ef67b04e134429b1e7fd
Z1906 <00@tedu.cn>
2019-09-03 09:02:09 +0800

    success1.0

--- js/system.js
+++ js/system.js
@@ -17,17 +17,18 @@ index.nav = {
         HTML += '    </ul>';
         HTML += '';
         HTML += '    <div class="act">';
-        HTML += '        <a href="">登入</a>';
+        HTML += '        <a href="./login.html">登入</a>';
         HTML += '        <a href="">注册</a>';
         HTML += '    </div>';
         HTML += '</div>';
         HTML += '</div>';
         return HTML;
     },
+    addr: ["index.html", "list.html", "pics.html", null, null, null, "send.html"],
     event: function () {
         var elemNavlis = $("#nav .con ul li");
         // console.log(elemNavlis[0]);
-        elemNavlis.each(function () {
+        elemNavlis.each(function (j, elem) {
             $(this).on("mouseover", function () {
                 // console.log($(this));
                 $(this).addClass("focus");
@@ -36,12 +37,29 @@ index.nav = {
                 // console.log($(this));
                 $(this).removeClass("focus");
             })
+            $(this).on("click", function () {
+                console.log(index.nav.addr);
+                $.each(index.nav.addr, function (i, item) {
+                    if (i == j) {
+                        console.log(item);
+                        window.location.href = item;
+                    }
+                })
+            })
+
         })
+    },
+    title: function (title) {
+        var HTML = '<div class="title">';
+        HTML += '<div class="ltip">' + title[0] + '</div>';
+        HTML += '<div class="rtip">' + title[1] + '</div>';
+        HTML += '</div>';
+        return HTML;
     }
 }
 
 index.imgsEvent = {
-    imgsSwitch: function (imgs, lis, elemImg) {
+    imgsSwitch: function (imgs, mags, lis, elemImg) {
         var n = -1
         lis.each(function (i, elem) {
             $(this).on("click", function () {
@@ -51,6 +69,7 @@ index.imgsEvent = {
                         $(lis[j]).addClass("focus");
                         // console.log(imgs[j]);
                         elemImg.attr("src", imgs[j]);
+                        elemImg.next().text(mags[j]);
                         n = j;
                     }
                     else {
@@ -64,4 +83,40 @@ index.imgsEvent = {
             lis[(++n) % imgs.length].click();
         }, 3000);
     }
+}
+
+index.articlelist = {
+    box: function () {
+        var HTML = '<div class="txtbox" data-scroll-reveal="bottom 5px 3 3">';
+        HTML += '<div class="tlttip">Python语言在人工智能（AI）中的优势</div>';
+        HTML += '<div class="tltframe">';
+        HTML += '<figure class="tltfleft">';
+        HTML += '<img src="images/toppic01.jpg" alt="" srcset="">';
+        HTML += '</figure>';
+        HTML += '<p class="tltfright">本文探讨了Python语言在AI领域的优势与运用。谁会成为AI和大数据时代的第一开发语言？</p>';
+        HTML += '</div>';
+        HTML += '</div>';
+        return HTML;
+    },
+    commendBox: function () {
+        var HTML = '<div class="trimgbox">';
+        HTML += '<figure class="triimg">';
+        HTML += '<img src="images/banner03.jpg" alt="">';
+        HTML += '</figure>';
+        HTML += ' </div>';
+
+        return HTML;
+    }
+}
+index.pics = {
+    html: function (imgurl, p1) {
+        var HTML = '<div class="item">'
+        HTML += '<figure>'
+        HTML += '<img width="270px" height="160px" src="' + imgurl + '" alt="">'
+        HTML += '</figure>'
+        HTML += '<p>' + p1 + '</p>'
+        HTML += '</div>';
+        return HTML;
+    }
+
 }
